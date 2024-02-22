3d27895f4b8eea38e8beeb0b80fe52aec90932be
Hamza Arif khan <HamzaArifkhan70@gmail.com>
2024-02-22 15:32:07 +0500

    delete api ready to be added

--- backend.js
+++ backend.js
@@ -1,21 +1,42 @@
-const express=require('express');
-const app=express();
-const port=3001;
+// Q: if i do git commit at different time and in the end add all of them?
 
-const user = [{
-    id: 35,  name: "Hamza Arif Khan", age: 28,
-    
-}]
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3001;
+
+const user = [
+  {
+    id: 35,
+    name: "Hamza Arif Khan",
+    age: 28,
+  },
+];
 // ======================----------------
 app.get("/show", (req, res) => {
-    res.json( user )
-})
-app.post("/add",(req,res)=>{
-    const newUser=req.body;
-    user.push(newUser)
-    res.send("user is added")
-})
+  res.json(user);
+});
+app.post("/add", (req, res) => {
+  const newUser = req.body;
+  user.push(newUser);
+  res.send(user);
+});
 
-app.listen(port,()=>{
-    console.log("Server is running on the port," ,port)
-})
+app.delete("/delete/:id", (req, res) => {
+  const userId = req.params.id;
+  console.log(userId)
+  user.forEach((element) => {
+      if (element.id == userId) {
+        console.log("to be delete",element)
+        // delete user[element]
+        user.splice(element,1)
+    };
+    });
+    console.log(user)
+    res.json(user);
+  
+});
+
+app.listen(port, () => {
+  console.log("Server is running on the port,", port);
+});
